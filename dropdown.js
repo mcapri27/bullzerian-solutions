ebc38a789b2adb1f4d0a342b2df09d7c07b6b709
mcapri27 <juanlyonne06@gmail.com>
2025-02-12 21:32:28 +0200

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -4,87 +4,87 @@
 
 function show1() { 
     document.getElementById('industry').setAttribute('placeholder', 'E-Commerce');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show2() { 
     document.getElementById('industry').setAttribute('placeholder', 'Healthcare');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show3() { 
     document.getElementById('industry').setAttribute('placeholder', 'Real-Estate');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show4() { 
     document.getElementById('industry').setAttribute('placeholder', 'Entertainment');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show5() { 
     document.getElementById('industry').setAttribute('placeholder', 'Travel & Hospitality');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show6() { 
     document.getElementById('industry').setAttribute('placeholder', 'Food & Beverage');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show7() { 
     document.getElementById('industry').setAttribute('placeholder', 'Non-Profit Organizations');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show8() { 
     document.getElementById('industry').setAttribute('placeholder', 'Technology');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show9() { 
     document.getElementById('industry').setAttribute('placeholder', 'Manufacturing');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show10() { 
     document.getElementById('industry').setAttribute('placeholder', 'Construction');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show11() { 
     document.getElementById('industry').setAttribute('placeholder', 'Automotive');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show12() { 
     document.getElementById('industry').setAttribute('placeholder', 'Retail');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show13() { 
     document.getElementById('industry').setAttribute('placeholder', 'Beauty & Wellness');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show14() { 
     document.getElementById('industry').setAttribute('placeholder', 'Sports & Fitness');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show15() { 
     document.getElementById('industry').setAttribute('placeholder', 'Media & Publishing');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show16() { 
     document.getElementById('industry').setAttribute('placeholder', 'Arts & Culture');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 }
 
 function show17() { 
     document.getElementById('industry').setAttribute('placeholder', 'Other');
-    dropdown.classList.toggle('active');
+    dropdown.classList.remove('active');
 } 
 
 let dropdown = document.querySelector('.industryDropdown');
